c3b8de04ec980618b2382ef73884baa85272f558
ichigoleite <picelboi@picelboi.xyz>
2026-09-04 12:06:22 -0400

    actually correctly patch oom_kill

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -361,21 +361,26 @@ void dump_tasks(const struct mem_cgroup *memcg, const nodemask_t *nodemask)
 	pr_info("[ pid ]   uid  tgid total_vm      rss nr_ptes swapents oom_score_adj name\n");
 	rcu_read_lock();
 	for_each_process(p) {
-                if (!process_shares_mm(p, mm))
-                        continue;
-                if (same_thread_group(p, victim))
-                        continue;
-		if (p->signal->oom_score_adj == OOM_SCORE_ADJ_MIN)
+		if (oom_unkillable_task(p, memcg, nodemask))
 			continue;
-		if (fatal_signal_pending(p))
+
+		task = find_lock_task_mm(p);
+		if (!task) {
+			/*
+			 * This is a kthread or all of p's threads have already
+			 * detached their mm's.  There's no need to report
+			 * them; they can't be oom killed anyway.
+			 */
 			continue;
-                if (unlikely(p->flags & PF_KTHREAD))
-                        continue;
-		task_lock(p);	/* Protect ->comm from prctl() */
-		pr_info("Kill process %d (%s) sharing same memory\n",
-			task_pid_nr(p), p->comm);
-		task_unlock(p);
-		do_send_sig_info(SIGKILL, SEND_SIG_FORCED, p, true);
+		}
+
+		pr_info("[%5d] %5d %5d %8lu %8lu %7ld %8lu         %5hd %s\n",
+			task->pid, from_kuid(&init_user_ns, task_uid(task)),
+			task->tgid, task->mm->total_vm, get_mm_rss(task->mm),
+			atomic_long_read(&task->mm->nr_ptes),
+			get_mm_counter(task->mm, MM_SWAPENTS),
+			task->signal->oom_score_adj, task->comm);
+		task_unlock(task);
 	}
 	rcu_read_unlock();
 }
@@ -508,18 +513,23 @@ void oom_kill_process(struct task_struct *p, gfp_t gfp_mask, int order,
 	 * pending fatal signal.
 	 */
 	rcu_read_lock();
-	for_each_process(p)
-		if (p->mm == mm && !same_thread_group(p, victim) &&
-		    !(p->flags & PF_KTHREAD)) {
-			if (p->signal->oom_score_adj == OOM_SCORE_ADJ_MIN)
-				continue;
-
-			task_lock(p);	/* Protect ->comm from prctl() */
-			pr_err("Kill process %d (%s) sharing same memory\n",
-				task_pid_nr(p), p->comm);
-			task_unlock(p);
-			do_send_sig_info(SIGKILL, SEND_SIG_FORCED, p, true);
-		}
+	for_each_process(p) {
+                if (!process_shares_mm(p, mm))
+                        continue;
+                if (same_thread_group(p, victim))
+                        continue;
+		if (p->signal->oom_score_adj == OOM_SCORE_ADJ_MIN)
+			continue;
+		if (fatal_signal_pending(p))
+			continue;
+                if (unlikely(p->flags & PF_KTHREAD))
+                        continue;
+		task_lock(p);	/* Protect ->comm from prctl() */
+		pr_info("Kill process %d (%s) sharing same memory\n",
+			task_pid_nr(p), p->comm);
+		task_unlock(p);
+		do_send_sig_info(SIGKILL, SEND_SIG_FORCED, p, true);
+	}
 	rcu_read_unlock();
 
 	set_tsk_thread_flag(victim, TIF_MEMDIE);
